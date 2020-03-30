87b539115d27f131ffebb5872c723bea11c076af
zhujianan <2494911084@qq.com>
2020-03-30 03:00:50 +0000

    个人中心页面

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-md-3">
+        <div class="card">
+          <img class="card-img-top" src="https://b-ssl.duitang.com/uploads/item/201608/27/20160827172726_GJfX2.jpeg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-text"><strong>个人简介</strong></h5>
+            <p class="card-text">kmlamldmalkdmadkalmdakdadllsmlk</p>
+            <hr>
+            <h5 class="card-text"><strong>注册于</strong></h5>
+            <p class="card-text">{{ $user->created_at->diffForHumans() }}</p>
+          </div>
+        </div>
+    </div>
+
+    <div class="col-md-9">
+        <div class="card">
+            <div class="card-body">
+                <h2>{{ $user->name }} <small>{{ $user->email }}</small></h2>
+            </div>
+        </div>
+        <hr>
+        <div class="card">
+            <div class="card-body">
+                <p>我的帖子，回复占位</p>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Auth::routes(['verify' => true]);
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
