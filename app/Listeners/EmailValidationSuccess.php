0d4ffb12020b52d5937f51caba8241cf177cf1ee
zhujianan <2494911084@qq.com>
2020-03-30 02:48:21 +0000

    邮箱验证成功后的提示,重置密码提示,退出登录

---
+++ app/Listeners/EmailValidationSuccess.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class EmailValidationSuccess
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Verified  $event
+     * @return void
+     */
+    public function handle(Verified $event)
+    {
+        session()->flash('status', '邮箱验证成功，您已成功登录');
+    }
+}
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,4 +1,4 @@
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm shadow bg-white rounded">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -26,23 +26,22 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                          <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="#">个人中心</a>
+                              <a class="dropdown-item" href="#">编辑资料</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item" href="#">
+                                  <form action="{{ route('logout') }}" method="post" onsubmit="return confirm('确认退出登录?');">
+                                      @csrf
+                                      <button class="btn btn-danger btn-block" type="submit">退出</button>
+                                  </form>
+                              </a>
+                            </div>
+                          </li>
                         @endguest
                     </ul>
                 </div>
